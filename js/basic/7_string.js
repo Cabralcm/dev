e037ee0d7947ab4405fa693b7499bb2e33587df6
cabralcm <christophercabral@gmail.com>
2021-05-05 02:14:45 -0400

    m

---
+++ js/basic/7_string.js
@@ -0,0 +1,28 @@
+
+
+
+function repeatStringNumTimes(str, num) {
+  var accumulatedStr = "";
+
+  while (num > 0) {
+    accumulatedStr += str;
+    num--;
+  }
+
+  return accumulatedStr;
+}
+
+// Recursive Method
+
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+function repeatString(str, num){
+	return num > 0 ? str + repeatString(str, num - 1) : ""; 
+}
+
